c797ad7e561c9e984580239e8a6f7746dda8a780
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-04 22:11:48 +0100

    Update present.py

--- present.py
+++ present.py
@@ -8,11 +8,8 @@ from itertools import zip_longest
 def main(scr):
     fr = Frame(scr.width, scr.height)
     anim = concat(
-            appear_left(scr, 1, "test", delay=1),
-            appear_left(scr, 2, "test", delay=2),
-            appear_left(scr, 3, "test", delay=3),
-            appear_left(scr, 4, "test", delay=4),
-            after(80, fadein(scr, 5, 0, "cool"))
+            after(0, fadein(scr, 5, 0, "cool")),
+            after(10, fadein(scr, 6, 0, "cool")),
             )
     # anim = cons(anim, after(3, left(scr, 4, "left", 0)))
     # input(list(anim))
@@ -20,7 +17,7 @@ def main(scr):
         for anim in changes:
             fr = anim(fr)
         fr.show(scr)
-        sleep(0.03)
+        sleep(0.01)
     sleep(1)
 
 Screen.wrapper(main)
