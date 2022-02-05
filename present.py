bb3474388f50b117f3b62a20ffed1c6b666c3904
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-05 01:12:57 +0100

    simple demo presentation

--- present.py
+++ present.py
@@ -7,12 +7,23 @@ from itertools import zip_longest
 
 def main(scr):
     fr = Frame(scr.width, scr.height)
+    # anim = concat(
+    #     appear(scr, 2, "un texte apparaît"),
+    #     appear(scr, 4, "un autre plus lentement", delay=9),
+    #     after(100, appear(scr, 0, "celui-ci ensuite")),
+    #     after(170, appear(scr, 2, "on remplace")),
+    #     after(200, appear_left(scr, 6, "youpi !", delay=2)),
+    # )
     anim = concat(
-            after(0, fadein(scr, 5, 0, "cool")),
-            after(10, fadein(scr, 6, 0, "cool")),
-            )
-    # anim = cons(anim, after(3, left(scr, 4, "left", 0)))
-    # input(list(anim))
+        appear(scr, 2, "un texte apparaît"),
+        appear(scr, 4, "un autre plus lentement", delay=9),
+        after(100, appear(scr, 0, "texte " + "très "*12 + "long" )),
+        after(170, appear(scr, 2, "on remplace ")),
+        after(200, appear_left(scr, 6, "youpi !", delay=2)),
+        after(200, appear_left(scr, 7, "youpi !", delay=3)),
+        after(200, appear_left(scr, 8, "youpi !", delay=4)),
+        after(200, appear_left(scr, 9, "youpi !", delay=5)),
+    )
     for changes in anim:
         for anim in changes:
             fr = anim(fr)
