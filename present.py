ca7943349edabf7a14d2e11bb5f209339cbe601c
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-05 17:30:50 +0100

    Update present.py

--- present.py
+++ present.py
@@ -1,23 +1,31 @@
 from asciimatics.screen import Screen
 from frame import Frame  # object representing a frame
 from animations import *  # animation functions
-from time import sleep
+from time import time, sleep
 from itertools import zip_longest
 
-def run_anim(anim, screen):
+
+def run_anim(screen, anim):
     fr = Frame(screen.width, screen.height)
     for changes in anim:
+        prev_frame_time = time()
         for anim in changes:
             fr = anim(fr)
         fr.show(screen)
-        sleep(0.01)
+        while time() - prev_frame_time < 0.01:
+            pass
     sleep(1)
 
-def main(scr):
+
+def main(s):
     anim = concat(
+        appear(s, 1, "voici une présentation"),
+        after(200,
+            appear(s, 4, "Elle est faîte avec ma super librairie python !")),
+        after(400,
+            appear_left(s, 6, ))
     )
-    run_anim(anim, scr)
+    run_anim(screen=s, anim=anim)
+
 
 Screen.wrapper(main)
-
-
