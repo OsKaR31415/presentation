d0fc1f7cc7c4a44111cd2030067ac43dea19c383
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-05 01:12:44 +0100

    add support for drawing lines

--- frame.py
+++ frame.py
@@ -1,6 +1,6 @@
 """Object representing a frame for a presentation.
 """
-
+from textwrap import wrap
 
 class Frame:
     def __init__(self, width, height):
@@ -20,7 +20,7 @@ class Frame:
                 screen.print_at(self.text[y][x], x, y, colour=self.color[y][x])
         screen.refresh()
 
-    def char_at(self, y: int, x: int, char: str, colour: int =0):
+    def char_at(self, y: int, x: int, char: str, color: int =255):
         """Set the character at the given position (with the given colour)."""
         # wrap arout the screen
         while x > self.width:
@@ -28,18 +28,29 @@ class Frame:
             y += 1
         # set the char in the char matrix
         self.text[y][x] = str(char)[0]  # only take the first character
-        self.color[y][x] = int(colour)
+        self.color[y][x] = int(color)
         return self
 
-    def str_at(self, y: int, x: int, string: str, color: int =0):
+    def str_at(self, y: int, x: int, string: str, color: int =255):
         """Put the given string at the given position."""
-        for pos, char in enumerate(string):
-            self.char_at(y, x+pos, char, color)
+        string = wrap(string, width=self.width)
+        for line_shift, line in enumerate(string):
+            for pos, char in enumerate(line):
+                self.char_at(y + line_shift, x+pos,
+                             char, color)
         return self
 
     def erase_line(self, y: int):
         """Erase the given line."""
         self.text[y] = [" " for _ in range(self.width)]
+        return self
+
+    def line(self, begin: (int, int), end: (int, int), char: str =None):
+        """Draw a straight line starting at *begin* coordinates and
+        ending and *end* coodinates."""
+        assert len(begin) == 2 and len(end) == 2  # both must be couples
+        self.move(*begin)
+        self.draw(*end)
         return self
 
 
