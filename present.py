5dd9ff785812d03206188c27393dcc1003c79a4b
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-05 17:20:37 +0100

    Update present.py

--- present.py
+++ present.py
@@ -4,32 +4,19 @@ from animations import *  # animation functions
 from time import sleep
 from itertools import zip_longest
 
-
-def main(scr):
-    fr = Frame(scr.width, scr.height)
-    # anim = concat(
-    #     appear(scr, 2, "un texte apparaît"),
-    #     appear(scr, 4, "un autre plus lentement", delay=9),
-    #     after(100, appear(scr, 0, "celui-ci ensuite")),
-    #     after(170, appear(scr, 2, "on remplace")),
-    #     after(200, appear_left(scr, 6, "youpi !", delay=2)),
-    # )
-    anim = concat(
-        appear(scr, 2, "un texte apparaît"),
-        appear(scr, 4, "un autre plus lentement", delay=9),
-        after(100, appear(scr, 0, "texte " + "très "*12 + "long" )),
-        after(170, appear(scr, 2, "on remplace ")),
-        after(200, appear_left(scr, 6, "youpi !", delay=2)),
-        after(200, appear_left(scr, 7, "youpi !", delay=3)),
-        after(200, appear_left(scr, 8, "youpi !", delay=4)),
-        after(200, appear_left(scr, 9, "youpi !", delay=5)),
-    )
+def run_anim(anim, screen):
+    fr = Frame(screen.width, screen.height)
     for changes in anim:
         for anim in changes:
             fr = anim(fr)
-        fr.show(scr)
+        fr.show(screen)
         sleep(0.01)
     sleep(1)
+
+def main(scr):
+    anim = concat(
+    )
+    run_anim(anim, scr)
 
 Screen.wrapper(main)
 
