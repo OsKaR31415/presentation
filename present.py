1b9e66851e530fce7152f77e6b590de53cd59e7b
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-05 17:51:14 +0100

    Update present.py

--- present.py
+++ present.py
@@ -19,11 +19,7 @@ def run_anim(screen, anim):
 
 def main(s):
     anim = concat(
-        appear(s, 1, "voici une présentation"),
-        after(200,
-            appear(s, 4, "Elle est faîte avec ma super librairie python !")),
-        after(400,
-            appear_left(s, 6, "youpiii"))
+        appear_up(s, center, )
     )
     run_anim(screen=s, anim=anim)
 
