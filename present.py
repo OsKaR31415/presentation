0ba0a56b7c94991c2f3c32a6989144b7dddbbf76
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-06 14:38:22 +0100

    Update present.py

--- present.py
+++ present.py
@@ -2,10 +2,9 @@ from asciimatics.screen import Screen
 from frame import Frame  # object representing a frame
 from animations import *  # animation functions
 from time import time, sleep
-from itertools import zip_longest
 
 
-def run_anim(screen, anim):
+def run_anim(screen, anim, post_delay: int = 100):
     fr = Frame(screen.width, screen.height)
     for changes in anim:
         prev_frame_time = time()
@@ -14,19 +13,22 @@ def run_anim(screen, anim):
         fr.show(screen)
         while time() - prev_frame_time < 0.01:
             pass
-    sleep(1)
+    sleep(post_delay/100)
 
 
-def main(s):
+def main(scr):
     anim = concat(
-        appear(s, 2, "un texte apparaîte"),
-        after(100, appear(s, 4, "un autre")),
-        after(200, appear(s, 2, "on le remplace")),
-        after(300, appear_left(s, 6, "youpi !!!", delay=2))
-        # *(after(3*y, center(s, y, "test")) for y in range(10)),
-        # *(after(3*y, erase_line(s, y-1)) for y in range(0, 10)),
+        one_by_one(
+            appear(scr, 2, "an appearing text"),
+            appear(scr, 4, "another appearing text"),
+            appear(scr, 2, "now we replace it"),
+            appear_left(scr, 6, "wooooh !", delay=3),
+            delay=50
+            )
     )
-    run_anim(screen=s, anim=anim)
+    anim = appear(scr, 2, "an appearing text")
+    run_anim(screen=scr, anim=anim)
 
 
 Screen.wrapper(main)
+
